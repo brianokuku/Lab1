bc787c7ed89336369db80889d87b846f76e00887
brianokuku <35316876+brianokuku@users.noreply.github.com>
2018-01-21 18:08:41 -0600

    Add files via upload

--- Lab5/app.js
+++ Lab5/app.js
@@ -11,9 +11,17 @@ var users = require('./routes/users');
 
 var app = express();
 
+var cache = require('memory-cache');
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
+
+// Requirements of the homework
+app.enable('case sensitive routing');
+app.set('strict routing', true);
+app.set('env', 'production');
+app.disable('x-powered-by');
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
@@ -26,6 +34,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
 app.use('/users', users);
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- Lab5/routes/users.js
+++ Lab5/routes/users.js
@@ -7,17 +7,16 @@ var ejs = require('ejs');
 router.get('/', function(req, res, next) {
   //This is in form of a Observable with Promise. It returns the entire object as an array
   
-  
   fetch('http://jsonplaceholder.typicode.com/users/')
     .then(function(res) {
         return res.text();
-    }).then(function(body) {
+    }).then(function(theData) {
         //res.send(body);
         //const theData = [{"name":"Brian Thomas", title:"This is the title 1"}, {"name":"Brian Smith 2", title:"This is the title 2"}];
-        console.log(body);
-        const theData = body;
+        console.log(theData);
+        
         res.render('index', {
-          show_data: theData});
+          show_data: JSON.parse(theData)});
     });
 
 });
