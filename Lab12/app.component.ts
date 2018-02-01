771bb4be618255ebac28d31e9c2d2004972e7080
brianokuku <35316876+brianokuku@users.noreply.github.com>
2018-01-31 18:59:08 -0600

    Add files via upload

--- Lab12/app.component.ts
+++ Lab12/app.component.ts
@@ -7,14 +7,8 @@ import { BrowserModule } from '@angular/platform-browser';
    
   <h2>Component with an unordered list</h2>
     <ul>
-      <li upperDirective *ngFor="let name of names">{{name}}</li>
-    </ul>
-
-  <h2>Boolean to hide visibility</h2>  
-    <div myVisibility>Hide and show me</div>
-
-    
-  `,
+      <li myVisibility showMe=true upperDirective *ngFor="let name of names" >{{name}}</li>
+    </ul>`,
   styles: ['']
 })
 
@@ -30,52 +24,3 @@ export class AppComponent {
 
 
 
-
-/*
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-root',
-  template: `
-    <div [ngClass]="{myColor:true, myBorder:false}">Directives</div>
-    <div [class.myBorder]="true">Directives</div>
-    <hr>
-    <div [ngStyle]="{'background-color':'yellow'}">Directives</div>
-    <hr>
-    <div myDirective1>Custom Directive</div><br>
-    <div myDirective1 shadow="1px 1px 5px red">Custom Directive</div>
-    <hr>
-    <h2>*ngIf</h2>
-    <div *ngIf="switch">Conditional Text</div>
-    <template *ngIf="switch">
-        <div>
-            Conditional Text
-        </div>
-    </template>
-    <button (click)="onSwitch()">Switch</button>
-    <hr>
-    <h2>*ngFor</h2>
-    <ul>
-        <li *ngFor="let item of items; let i = index">{{item}} - (Index: {{i}})</li>
-    </ul>
-    <hr>
-    <h2>ngSwitch</h2>
-    <div [ngSwitch]="value">
-      <p *ngSwitchCase="10">10</p>
-      <p *ngSwitchCase="100">100</p>
-      <p *ngSwitchDefault>Default</p>
-    </div>
-  `,
-  styles: ['.myBorder { border: 2px solid red; }',
-           '.myColor  { color: green; }']
-})
-export class AppComponent {
-    private switch = true;
-    private items = [1, 2, 3, 4, 5];
-    private value = 10;
-    onSwitch() {
-        this.switch = !this.switch;
-    }
-}
-
-*/
